cd85b2d546d8c5fab36d2fce6a154665f0c836d7
Andrew Thai <37123002@edu.cbe.ab.ca>
2024-10-21 15:27:19 -0600

    e

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ server.use(express.json())
 
 //All your code goes here
 let activeSessions={}
+server.use(express.static('public'))
+server.get("/newgame", (req,res)=>{
+    
+})
+server.get("/gamestate", (req,res)=>{
+    
+})
+server.post("/guess", (req,res)=>{
+    
+})
+server.delete("/reset", (req,res)=>{
+    
+})
+server.delete("/delete", (req,res)=>{
+    
+})
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
