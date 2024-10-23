97143d8734c2075a51938a6c78281adfa178e79f
Andrew Thai <37123002@edu.cbe.ab.ca>
2024-10-23 15:28:12 -0600

    post

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ server.use(express.static('public'))
 let activeSessions={}
 server.get("/newgame", (req,res)=>{
     let setWord = req.query.answer
-    let generated="niger"
+    let generated="apple"
     let over=false
-    if(setWord==generated){
-        over=true
-    }
     if(!setWord||setWord.length!=5){
         setWord=generated
     }
@@ -23,7 +20,7 @@ server.get("/newgame", (req,res)=>{
         closeLetters: [],
         rightLetters: [],
         remainingGuesses: 6,
-        gameOver: over
+        gameOver: over,
     }
     activeSessions[newID] = newGame
     res.status(201)
@@ -45,9 +42,63 @@ server.get("/gamestate", (req,res)=>{
         }
     }
 })
-// server.post("/guess", (req,res)=>{
-    
-// })
+server.post("/guess", (req,res)=>{
+    let ID=req.body.sessionID
+    let guess=req.body.guess
+    if(!ID){
+        res.status(400)
+        res.send({error: "id is missing"})
+    }else{
+        if(activeSessions[ID]){
+            let game=activeSessions[ID]
+            let guessCheck=guess.split("")
+            if(guessCheck.length!=5){
+                res.status(400)
+                res.send({error:"guess is not 5 letters long"})
+            }
+            let word=game.wordToGuess.split("")
+            let guessArr=[]
+            game.remainingGuesses-=1
+            if(guess==game.wordToGuess){
+                game.gameOver=true
+                game.rightLetters=guessCheck
+            }else{
+                for (let i = 0; i < guessCheck.length; i++) {
+                    let included=false
+                    let guessObj={
+                        value:guessCheck[i]
+                    }
+                    for (let j = 0; j < word.length; j++) {
+                        if(word[j]==guessCheck[i]){
+                            if(j==i){
+                                game.rightLetters.push(guessCheck[i])
+                                guessObj.result="RIGHT"
+                            }else{
+                                game.closeLetters.push(guessCheck[i])
+                                guessObj.result="CLOSE"
+                            }
+                            included=true
+                        }
+                    }
+                    if(included==false){
+                        game.wrongLetters.push(guessCheck[i])
+                        guessObj.result="WRONG"
+                    }
+                    guessArr.push(guessObj)
+                }
+            }
+            if(game.remainingGuesses==0){
+                game.gameOver=true
+            }
+            game.guesses.push(guessArr)
+            res.status(201)
+            res.send({gameState: game})
+        }else{
+            res.status(404)
+            res.send({error: "game doesn't exist"})
+        }
+    }
+})
 // server.delete("/reset", (req,res)=>{
     
 // })
